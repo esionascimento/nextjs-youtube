2f3092cbbd7a890be7ec10df3e5237038bf6f388
esionascimento <esionascimento@protonmail.com>
2022-04-14 17:20:42 -0300

    refactor styled index Layout

--- src/components/Layout/styled.ts
+++ src/components/Layout/styled.ts
@@ -1,13 +1,5 @@
 import { styled } from "@mui/system"
 
-
-export const roots = {
-  display: "flex",
-  height: "100vh",
-  overflow: "hidden",
-  width: "100vw"
-}
-
 export const StatWrapper = styled("div")(
   ({ theme }) => `
   background-color: ${theme.palette.background.paper};
@@ -15,5 +7,26 @@ export const StatWrapper = styled("div")(
   height: 100vh;
   overflow: hidden;
   width: 100vw;
-`,
-)
+`)
+
+export const wrapper = styled("div")(
+  ({ theme }) => `
+  display: flex;
+  flex: 1 1 auto;
+  overflow: hidden;
+  paddingTop: 64,
+  [${theme.breakpoints.up("lg")}]: {
+    paddingLeft: 256
+  }
+`)
+
+export const contentContainer = styled("div")`
+  display: flex;
+  flex: 1 1 auto;
+  overflow: hidden
+`
+export const content = styled("div")`
+  flex: 1 1 auto;
+  height: 100 %;
+  overflow: auto;
+`
