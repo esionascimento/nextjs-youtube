2ad1d3e0a98f777bac6e83a9bd2f5e533e3f1041
esionascimento <esionascimento@protonmail.com>
2022-04-15 23:02:10 -0400

    import mui/icons-material | create logo

--- src/components/Layout/TopBar.tsx
+++ src/components/Layout/TopBar.tsx
@@ -1,6 +1,8 @@
 import AppBar from "@mui/material/AppBar"
 import Toolbar from "@mui/material/Toolbar"
 import { styled } from "@mui/material/styles"
+import Box from "@mui/material/Box"
+import MenuIcon from "@mui/icons-material/Menu"
 
 const SAppBar = styled(AppBar)(({ theme }) => ({
   boxShadow: "none",
@@ -8,12 +10,30 @@ const SAppBar = styled(AppBar)(({ theme }) => ({
   backgroundColor: theme.palette.background.default
 }))
 
+const SToolbar = styled(Toolbar)({
+  display: "flex",
+  minHeight: 56,
+  alignItems: "center",
+  justifyContent: "space-between"
+})
+
+const SImg = styled("img")(({ theme }) => ({
+  cursor: "pointer",
+  height: 18,
+  marginLeft: theme.spacing(3)
+}))
+
 const TopBar = () => {
   return (
     <SAppBar color="default">
-      <Toolbar>
-        appBar
-      </Toolbar>
+      <SToolbar>
+        <Box display="flex" alignItems="center">
+          <MenuIcon />
+          <SImg src="./new-youtube-logo.svg" alt="logo" />
+        </Box>
+        <Box></Box>
+        <Box></Box>
+      </SToolbar>
     </SAppBar>
   )
 }
