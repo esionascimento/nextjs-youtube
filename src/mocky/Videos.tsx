67ba44e4f540b9a7842c6d093549d2d5cdb3ebdd
esionascimento <esionascimento@protonmail.com>
2022-04-16 19:27:14 -0400

    create file mocky data

---
+++ src/mocky/Videos.tsx
@@ -0,0 +1,79 @@
+export const dataMocky = [
+  {
+    id: 1,
+    title: "NEXT.JS",
+    authorId: 1,
+    authorName: "Lucas Silveira",
+    authorAvatar: "avatarUrl",
+    views: 11,
+    thumb: "/thumbs/next1",
+    videoUrl: "url",
+    updateAt: new Date()
+  },
+  {
+    id: 2,
+    title: "Modulo 02",
+    authorId: 2,
+    authorName: "Lucas Silveira",
+    authorAvatar: "avatarUrl",
+    views: 22,
+    thumb: "/thumbs/next1",
+    videoUrl: "url",
+    updateAt: new Date()
+  },
+  {
+    id: 3,
+    title: "Modulo 03",
+    authorId: 3,
+    authorName: "Lucas Silveira",
+    authorAvatar: "avatarUrl",
+    views: 33,
+    thumb: "/thumbs/next1",
+    videoUrl: "url",
+    updateAt: new Date()
+  },
+  {
+    id: 4,
+    title: "Modulo 04",
+    authorId: 2,
+    authorName: "Lucas Silveira",
+    authorAvatar: "avatarUrl",
+    views: 44,
+    thumb: "/thumbs/next1",
+    videoUrl: "url",
+    updateAt: new Date()
+  },
+  {
+    id: 5,
+    title: "Modulo 04",
+    authorId: 2,
+    authorName: "Lucas Silveira",
+    authorAvatar: "avatarUrl",
+    views: 44,
+    thumb: "/thumbs/next1",
+    videoUrl: "url",
+    updateAt: new Date()
+  },
+  {
+    id: 6,
+    title: "Modulo 04",
+    authorId: 2,
+    authorName: "Lucas Silveira",
+    authorAvatar: "avatarUrl",
+    views: 44,
+    thumb: "/thumbs/next1",
+    videoUrl: "url",
+    updateAt: new Date()
+  },
+  {
+    id: 7,
+    title: "Modulo 04",
+    authorId: 2,
+    authorName: "Lucas Silveira",
+    authorAvatar: "avatarUrl",
+    views: 44,
+    thumb: "/thumbs/next1",
+    videoUrl: "url",
+    updateAt: new Date()
+  }
+]
